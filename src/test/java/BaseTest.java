a0afd7df757ffe23a5d7c36bdb241ef11d55461f
Azat <azat.zakuanov@yahoo.com>
2020-03-28 12:40:59 -0700

    Selenium Grid + Sauce + BS

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -5,13 +5,26 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.CapabilityType;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.concurrent.TimeUnit;
 
 public class BaseTest {
+
+    public static final String USERNAME = "azatersky";
+    public static final String ACCESS_KEY = "7bb293f8-a613-461e-a326-faf59a4a0dcd";
+    public static final String URL = "http://" + USERNAME + ":" + ACCESS_KEY + "@ondemand.saucelabs.com:80/wd/hub";
+    public static final String LOCALGRIDURL = "http://localhost:4444/wd/hub";
+
+    public static final String BROWSERSTACKUSERNAME = "azat35";
+    public static final String BROWSERSTACKACCESS_KEY = "qmDeqGhqmM3Xpqu6LCo4";
+    public static final String BROWSERSTACKURL = "http://" + BROWSERSTACKUSERNAME + ":" + BROWSERSTACKACCESS_KEY + "@hub-cloud.browserstack.com/wd/hub";
+
 
     WebDriver driver;
     WebDriverWait wait;
@@ -19,12 +32,14 @@ public class BaseTest {
     LoginPage loginPage;
 
     @BeforeMethod
-    public void setUp(){
+    public void setUp() throws MalformedURLException {
 
         ChromeOptions options = new ChromeOptions();
         options.addArguments("ignore-certificate-errors");
 
-        driver = new ChromeDriver(options);
+//        driver = new ChromeDriver(options);
+
+        driver = new RemoteWebDriver(new URL(LOCALGRIDURL), options);
 
 
         driver.manage().window().maximize();
